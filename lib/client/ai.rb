2a91d8e2eac6725af4e9f78cfeb9dede2a798e59
Thiago May <may.thiago@proton.me>
2024-10-10 21:52:21 -0300

    Increase timeout and hide button

--- lib/client/ai.rb
+++ lib/client/ai.rb
@@ -1,7 +1,10 @@
 module Client
   class Ai
+    include HTTParty
+    default_timeout 120
+
     def self.generate(content)
-      response = HTTParty.post('http://localhost:11434/api/generate', body: {
+      response = post('http://localhost:11434/api/generate', body: {
                                  model: 'llama3.2',
                                  prompt: prompt(content),
                                  stream: false
@@ -12,9 +15,11 @@ module Client
     end
 
     def self.prompt(content)
-<<PROMPT
-Você é uma ferramenta que faz resumo de noticias para um usuário mobile. Resuma esta notícia de forma clara e simples, destacando os fatos mais importantes e explicando o contexto.
-Noticia: #{content}
+      sanitized_content = ActionView::Base.full_sanitizer.sanitize(content)
+      puts sanitized_content
+      <<PROMPT
+Noticia: #{sanitized_content}
+Resuma esta notícia de forma clara e simples, destacando os fatos mais importantes e explicando o contexto.
 PROMPT
     end
   end
